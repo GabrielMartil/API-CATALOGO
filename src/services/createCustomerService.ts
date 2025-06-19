56b6f201712b30558783aa7ba8e50da88911cb32
Gabriel Martil <bicodeiro@gmail.com>
2025-06-19 19:09:25 -0300

    Atualização no updateCustomerController

--- src/services/createCustomerService.ts
+++ src/services/createCustomerService.ts
@@ -20,7 +20,7 @@ class CreateCustomerService {
         name_loja,
         whatsapp_num,
         instagram_name,
-        whatsapp_status: whatsapp_status as Status, // converte string para enum
+        whatsapp_status: whatsapp_status as Status,
         instagram_status: instagram_status as Status
       },
     });
