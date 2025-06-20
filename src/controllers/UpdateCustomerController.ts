13967efaa739b8ce58ec74431531a36d14c00937
Gabriel Martil <bicodeiro@gmail.com>
2025-06-19 21:08:18 -0300

    Atualização da API

--- src/controllers/UpdateCustomerController.ts
+++ src/controllers/UpdateCustomerController.ts
@@ -23,12 +23,8 @@ class UpdateCustomerController {
         instagram_status
       } = request.body as UpdateCustomerProps;
 
-      if (!id) {
-        return reply.status(400).send({ error: "ID é obrigatório para atualização." });
-      }
-
       const configuracaoAtualizada = await prismaClient.configuracao.update({
-        where: { id: "6854a45b7bf195e6a4d31726" },
+        where: { id: "6854a637e38f1e4a3f3331b6" },
         data: {
           ...(name_loja && { name_loja }),
           ...(whatsapp_num && { whatsapp_num }),
